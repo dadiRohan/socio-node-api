2f262fa99ee23ac1d5e7c76bca20254a97684563
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2025-05-28 12:36:39 +0530

    Like module update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
+const likeRoutes = require('./routes/likeRoutes');
 const cors = require('cors');
 dotenv.config();
 connectDB();
@@ -22,6 +23,8 @@ app.use('/api/profiles', userRoutes);
 app.use('/api/posts', postRoutes);
 
 app.use('/api/comments', commentRoutes);
+
+app.use('/api/likes', likeRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Not Found' });
